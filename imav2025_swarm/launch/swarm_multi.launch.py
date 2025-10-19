fcc7d08439f87e42ea0d3d24107f3189130f45dc
Karolina Kozubik <k4aro0@gmail.com>
2025-10-19 15:29:11 +0200

    feat: ns_prefix in multi launch

--- imav2025_swarm/launch/swarm_multi.launch.py
+++ imav2025_swarm/launch/swarm_multi.launch.py
@@ -12,9 +12,11 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     imav_package_path = get_package_share_path("imav2025_swarm")
 
+    ns_prefix = LaunchConfiguration("ns_prefix")
     swarm_count = LaunchConfiguration("swarm_count")
     params_file = LaunchConfiguration("params_file")
 
+    ns_prefix_arg = DeclareLaunchArgument("ns_prefix", default_value="uav")
     swarm_count_arg = DeclareLaunchArgument("swarm_count")
     params_file_arg = DeclareLaunchArgument(
         "params_file",
@@ -30,6 +32,7 @@ def generate_launch_description():
                     (imav_package_path / "launch" / "swarm_member.launch.py").as_posix()
                 ),
                 launch_arguments={
+                    "ns_prefix": ns_prefix,
                     "drone_id": str(drone_id + 1),
                     "params_file": params_file,
                 }.items(),
@@ -39,6 +42,7 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
+    ld.add_action(ns_prefix_arg)
     ld.add_action(swarm_count_arg)
     ld.add_action(params_file_arg)
     ld.add_action(OpaqueFunction(function=actions_with_context))
